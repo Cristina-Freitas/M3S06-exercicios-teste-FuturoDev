0250801b0ed7ab76c9c267b6d7750cada9a9f1ca
CrisTinaFLN <crisjf.tina@hotmail.com>
2023-07-09 21:52:14 -0300

    [M3S06] Ex 8 - Crie os testes unitários para os métodos detelaProdutoPorId

--- src/test/java/com/example/exerciciotestes/service/ProdutoServiceTest.java
+++ src/test/java/com/example/exerciciotestes/service/ProdutoServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 import java.util.Optional;
@@ -92,5 +93,11 @@ class ProdutoServiceTest {
 
     @Test
     void detelaProdutoPorId() {
+        Long produtoId = 1L;
+        when(produtoRepository.findById(produtoId)).thenReturn(Optional.empty());
+
+        assertThrows(HttpClientErrorException.class, () -> produtoService.detelaProdutoPorId(produtoId));
+        verify(produtoRepository, times(1)).findById(produtoId);
+        verify(produtoRepository, never()).deleteById(produtoId);
     }
 }
